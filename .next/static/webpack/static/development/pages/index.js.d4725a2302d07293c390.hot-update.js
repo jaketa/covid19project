7bc3fcfbdfa28b59f263554eacf85d2947d54b09
jaketa <jake.t.ashley@gmail.com>
2020-04-09 00:28:48 -0400

    Mockup

---
+++ .next/static/webpack/static/development/pages/index.js.d4725a2302d07293c390.hot-update.js
@@ -0,0 +1,237 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Navbar.js":
+/*!******************************!*\
+  !*** ./components/Navbar.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _this = undefined,
+    _jsxFileName = "/Users/Jake/Desktop/c19-project/components/Navbar.js";
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+var Navbar = function Navbar() {
+  return __jsx("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2,
+      columnNumber: 3
+    }
+  }, __jsx("div", {
+    "class": "bg-black",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3,
+      columnNumber: 5
+    }
+  }, __jsx("div", {
+    "class": "container mx-auto px-4",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4,
+      columnNumber: 7
+    }
+  }, __jsx("div", {
+    "class": "flex items-center md:justify-between py-4",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 9
+    }
+  }, __jsx("div", {
+    "class": "w-1/4 md:hidden",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6,
+      columnNumber: 11
+    }
+  }, __jsx("svg", {
+    "class": "fill-current text-white h-8 w-8",
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 0 20 20",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7,
+      columnNumber: 13
+    }
+  }, __jsx("path", {
+    d: "M16.4 9H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1zm0 4H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1zM3.6 7h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1z",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12,
+      columnNumber: 15
+    }
+  }))), __jsx("div", {
+    "class": "w-1/2 md:w-auto text-center text-white text-2xl font-medium",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15,
+      columnNumber: 11
+    }
+  }, "Social Distributing"), __jsx("div", {
+    "class": "w-1/4 md:w-auto md:flex text-right",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18,
+      columnNumber: 11
+    }
+  }, __jsx("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19,
+      columnNumber: 13
+    }
+  }, __jsx("img", {
+    "class": "inline-block h-8 w-8 rounded-full",
+    src: "https://avatars0.githubusercontent.com/u/4323180?s=460&v=4",
+    alt: "",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20,
+      columnNumber: 15
+    }
+  })), __jsx("div", {
+    "class": "hidden md:block md:flex md:items-center ml-2",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 26,
+      columnNumber: 13
+    }
+  }, __jsx("span", {
+    "class": "text-white text-sm mr-1",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 27,
+      columnNumber: 15
+    }
+  }, "Adam Wathan"), __jsx("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 28,
+      columnNumber: 15
+    }
+  }, __jsx("svg", {
+    "class": "fill-current text-white h-4 w-4 block opacity-50",
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 0 20 20",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29,
+      columnNumber: 17
+    }
+  }, __jsx("path", {
+    d: "M4.516 7.548c.436-.446 1.043-.481 1.576 0L10 11.295l3.908-3.747c.533-.481 1.141-.446 1.574 0 .436.445.408 1.197 0 1.615-.406.418-4.695 4.502-4.695 4.502a1.095 1.095 0 0 1-1.576 0S4.924 9.581 4.516 9.163c-.409-.418-.436-1.17 0-1.615z",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 34,
+      columnNumber: 19
+    }
+  })))))))), __jsx("div", {
+    "class": "hidden bg-blue-dark md:block md:bg-white md:border-b",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 42,
+      columnNumber: 5
+    }
+  }, __jsx("div", {
+    "class": "container mx-auto px-4",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43,
+      columnNumber: 7
+    }
+  }, __jsx("div", {
+    "class": "md:flex",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 44,
+      columnNumber: 9
+    }
+  }, __jsx("div", {
+    "class": "flex -mb-px mr-8",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 45,
+      columnNumber: 11
+    }
+  }, __jsx("a", {
+    href: "#",
+    "class": "no-underline text-white md:text-blue-dark flex items-center py-4 border-b border-blue-dark",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 46,
+      columnNumber: 13
+    }
+  }, "Dashboard")), __jsx("div", {
+    "class": "flex -mb-px",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 53,
+      columnNumber: 11
+    }
+  }, __jsx("a", {
+    href: "#",
+    "class": "no-underline text-white opacity-50 md:text-grey-dark md:opacity-100 flex items-center py-4 border-b border-transparent hover:opacity-100 md:hover:border-grey-dark",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 54,
+      columnNumber: 13
+    }
+  }, __jsx("svg", {
+    "class": "h-6 w-6 fill-current mr-2",
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 0 24 24",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 58,
+      columnNumber: 15
+    }
+  }, __jsx("path", {
+    d: "M18.783 12c0-1.049.646-1.875 1.617-2.443a8.932 8.932 0 0 0-.692-1.672c-1.089.285-1.97-.141-2.711-.883-.741-.74-.968-1.621-.683-2.711a8.732 8.732 0 0 0-1.672-.691c-.568.97-1.595 1.615-2.642 1.615-1.048 0-2.074-.645-2.643-1.615-.58.172-1.14.403-1.671.691.285 1.09.059 1.971-.684 2.711-.74.742-1.621 1.168-2.711.883A8.797 8.797 0 0 0 3.6 9.557c.97.568 1.615 1.394 1.615 2.443 0 1.047-.645 2.074-1.615 2.643.173.58.404 1.14.691 1.672 1.09-.285 1.971-.059 2.711.682.741.742.969 1.623.684 2.711.532.288 1.092.52 1.672.693.568-.973 1.595-1.617 2.643-1.617 1.047 0 2.074.645 2.643 1.617a8.963 8.963 0 0 0 1.672-.693c-.285-1.088-.059-1.969.683-2.711.741-.74 1.622-1.166 2.711-.883.287-.532.52-1.092.692-1.672-.973-.569-1.619-1.395-1.619-2.442zM12 15.652a3.653 3.653 0 1 1 0-7.306 3.653 3.653 0 0 1 0 7.306z",
+    "fill-rule": "nonzero",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 63,
+      columnNumber: 17
+    }
+  })), "Settings"))))));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Navbar);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.d4725a2302d07293c390.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,254 @@
+import Layout from "../components/Layout";
+
+export default function Index() {
+  return (
+    <Layout>
+      <div class="flex-grow container mx-auto sm:px-4 pt-6 pb-8">
+        <div class="bg-white border-t border-b sm:border-l sm:border-r sm:rounded shadow mb-6">
+          <div class="border-b px-6 bg-black">
+            <div class="flex justify-between -mb-px">
+              <div class="hidden lg:flex">
+                <button
+                  type="button"
+                  class="appearance-none py-4 text-gray-100 border-b border-red mr-6"
+                >
+                  Open Requests
+                </button>
+                <button
+                  type="button"
+                  class="appearance-none py-4 text-gray-500 border-b border-transparent hover:border-grey-dark mr-6"
+                >
+                  Request History
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="flex items-center px-6 lg:hidden">
+            <div class="flex-grow flex-no-shrink py-6">
+              <div class="text-grey-darker mb-2">
+                <span class="text-3xl align-top">CA$</span>
+                <span class="text-5xl">21,404</span>
+                <span class="text-3xl align-top">.74</span>
+              </div>
+              <div class="text-green-light text-sm">
+                &uarr; CA$12,955.35 (154.16%)
+              </div>
+            </div>
+            <div class="flex-shrink w-32 inline-block relative">
+              <select class="block appearance-none w-full bg-white border border-grey-light px-4 py-2 pr-8 rounded">
+                <option>BTC</option>
+                <option>ETH</option>
+                <option>LTC</option>
+              </select>
+              <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey">
+                <svg
+                  class="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex-grow flex px-6 py-6 text-grey-darker items-center border-b -mx-4">
+                <div class="w-2/5 xl:w-1/4 px-4 flex items-center">
+                  <div class="rounded-full bg-grey inline-flex mr-3">
+                    <svg
+                      class="fill-current text-white h-8 w-8 block"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 38 38"
+                    >
+                      <g fill-rule="evenodd">
+                        <path d="M12.29 28.04l1.29-5.52-1.58.67.63-2.85 1.64-.68L16.52 10h5.23l-1.52 7.14 2.09-.74-.58 2.7-2.05.8-.9 4.34h8.1l-.99 3.8z"></path>
+                      </g>
+                    </svg>
+                  </div>
+                  <span class="font-bold">Requested Item 2</span>
+                </div>
+                <div class="hidden md:flex lg:hidden xl:flex w-1/4 px-4 items-center">
+                  <div class="bg-grey h-2 w-2 rounded-full mr-2"></div>
+                  Claimed 03/26/2020
+                </div>
+                <div class="flex w-3/5 md:w/12 items-center">
+                  <div class="w-1/2 px-4">
+                    <div class="text-right">New</div>
+                  </div>
+                  <div class="w-1/2 px-4">
+                    <div class="text-right text-grey">
+                      <button class="bg-red-500 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded">
+                        Unclaim
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="flex-grow flex px-6 py-6 text-grey-darker items-center border-b -mx-4">
+                <div class="w-2/5 xl:w-1/4 px-4 flex items-center">
+                  <div class="rounded-full bg-grey inline-flex mr-3">
+                    <svg
+                      class="fill-current text-white h-8 w-8 block"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 38 38"
+                    >
+                      <g fill-rule="evenodd">
+                        <path d="M12.29 28.04l1.29-5.52-1.58.67.63-2.85 1.64-.68L16.52 10h5.23l-1.52 7.14 2.09-.74-.58 2.7-2.05.8-.9 4.34h8.1l-.99 3.8z"></path>
+                      </g>
+                    </svg>
+                  </div>
+                  <span class="font-bold">Requested Item 2</span>
+                </div>
+                <div class="hidden md:flex lg:hidden xl:flex w-1/4 px-4 items-center">
+                  <div class="bg-grey h-2 w-2 rounded-full mr-2"></div>
+                  Claimed 03/26/2020
+                </div>
+                <div class="flex w-3/5 md:w/12 items-center">
+                  <div class="w-1/2 px-4">
+                    <div class="text-right">In Process</div>
+                  </div>
+                  <div class="w-1/2 px-4">
+                    <div class="text-right text-grey">
+                      <button class="bg-red-500 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded">
+                        Unclaim
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>
+        <div
+          class="bg-blue-100 border border-blue-400 text-blue-700 px-4 py-3 rounded relative mb-6"
+          role="alert"
+        >
+          <strong class="font-bold">Hey! </strong>
+          <span class="block sm:inline">
+            You can have only two open requests at any given time. To claim a
+            new item, you must first release one item from your queue.
+          </span>
+          <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg
+              class="fill-current h-6 w-6 text-blue-500"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
+        </div>
+        <div class="flex flex-wrap -mx-4">
+          <div class="w-full mb-6 lg:mb-0 px-4 flex flex-col">
+            <div class="flex-grow flex flex-col bg-white border-t border-b sm:rounded sm:border shadow overflow-hidden">
+              <div class="border-b">
+                <div class="flex justify-between px-6 -mb-px bg-gray-300">
+                  <h3 class="text-black py-4 font-normal text-lg">Unclaimed</h3>
+                </div>
+              </div>
+              <div class="flex-grow flex px-6 py-6 text-grey-darker items-center border-b -mx-4">
+                <div class="w-2/5 xl:w-1/4 px-4 flex items-center">
+                  <div class="rounded-full bg-grey inline-flex mr-3">
+                    <svg
+                      class="fill-current text-white h-8 w-8 block"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 38 38"
+                    >
+                      <g fill-rule="evenodd">
+                        <path d="M12.29 28.04l1.29-5.52-1.58.67.63-2.85 1.64-.68L16.52 10h5.23l-1.52 7.14 2.09-.74-.58 2.7-2.05.8-.9 4.34h8.1l-.99 3.8z"></path>
+                      </g>
+                    </svg>
+                  </div>
+                  <span class="font-bold">Available Item 1</span>
+                </div>
+                <div class="hidden md:flex lg:hidden xl:flex w-1/4 px-4 items-center">
+                  <div class="bg-grey h-2 w-2 rounded-full mr-2"></div>
+                  Added 04/02/2020
+                </div>
+                <div class="flex w-3/5 md:w/12 items-center">
+                  <div class="w-1/2 px-4">
+                    <div class="text-right">by Provider A</div>
+                  </div>
+                  <div class="w-1/2 px-4">
+                    <div class="text-right text-grey">
+                      <button class="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded">
+                        Claim
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="flex-grow flex px-6 py-6 text-grey-darker items-center border-b -mx-4">
+                <div class="w-2/5 xl:w-1/4 px-4 flex items-center">
+                  <div class="rounded-full bg-grey inline-flex mr-3">
+                    <svg
+                      class="fill-current text-white h-8 w-8 block"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 38 38"
+                    >
+                      <g fill-rule="evenodd">
+                        <path d="M12.29 28.04l1.29-5.52-1.58.67.63-2.85 1.64-.68L16.52 10h5.23l-1.52 7.14 2.09-.74-.58 2.7-2.05.8-.9 4.34h8.1l-.99 3.8z"></path>
+                      </g>
+                    </svg>
+                  </div>
+                  <span class="font-bold">Available Item 2</span>
+                </div>
+                <div class="hidden md:flex lg:hidden xl:flex w-1/4 px-4 items-center">
+                  <div class="bg-grey h-2 w-2 rounded-full mr-2"></div>
+                  Added 03/26/2020
+                </div>
+                <div class="flex w-3/5 md:w/12 items-center">
+                  <div class="w-1/2 px-4">
+                    <div class="text-right">by Provider B</div>
+                  </div>
+                  <div class="w-1/2 px-4">
+                    <div class="text-right text-grey">
+                      <button class="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded">
+                        Claim
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="flex-grow flex px-6 py-6 text-grey-darker items-center border-b -mx-4">
+                <div class="w-2/5 xl:w-1/4 px-4 flex items-center">
+                  <div class="rounded-full bg-indigo inline-flex mr-3">
+                    <svg
+                      class="fill-current text-white h-8 w-8 block"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 32 32"
+                    >
+                      <g fill-rule="evenodd">
+                        <path d="M10.13 17.76c-.1-.15-.06-.2.09-.12l5.49 3.09c.15.08.4.08.56 0l5.58-3.08c.16-.08.2-.03.1.11L16.2 25.9c-.1.15-.28.15-.38 0l-5.7-8.13zm.04-2.03a.3.3 0 0 1-.13-.42l5.74-9.2c.1-.15.25-.15.34 0l5.77 9.19c.1.14.05.33-.12.41l-5.5 2.78a.73.73 0 0 1-.6 0l-5.5-2.76z"></path>
+                      </g>
+                    </svg>
+                  </div>
+                  <span class="font-bold">Available Item 3</span>
+                </div>
+                <div class="hidden md:flex lg:hidden xl:flex w-1/4 px-4 items-center">
+                  <div class="bg-indigo h-2 w-2 rounded-full mr-2"></div>
+                  Added 03/26/2020
+                </div>
+                <div class="flex w-3/5 md:w/12 items-center">
+                  <div class="w-1/2 px-4">
+                    <div class="text-right">by Provider C</div>
+                  </div>
+                  <div class="w-1/2 px-4">
+                    <div class="text-right text-grey">
+                      <button class="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded">
+                        Claim
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="px-6 py-4 bg-gray-100">
+                <div class="text-center text-gray-500">Load More...</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
